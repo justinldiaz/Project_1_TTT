feb1913e761290500962d4810a04f1282b239f7b
Justin <justindiaz91@gmail.com>
2015-05-07 19:53:45 -0700

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-// OOP Tic Tac Toe boilerplate code
-
+// OOP Tic Tac Toe boilerplate codewindow.addEventListener('load', function() {
 // Execute this code only AFTER the document is ready
 // Hint: use jQuery's `$(document).ready`
+  var board = $('body');
+  var squares = $('.square p');
+  var squareBorders = $('.square');
+  var reset = $('.reset');
+  var playCount = 0;
+  var xConsecWins = 0;
+  var oConsecWins = 0;
 
   function Game() {
     //Create a new instance of player 1
@@ -13,25 +19,29 @@
     //Create the board
     //this.board = ...
   }
-
+  function Player() {
+    this.team = X || O
+    //Is the player X or O?
+    //this.team = ... 
+  
   // Remember: prototypes are shared functions between all game instances
   Game.prototype.nextPlayer = function() {
     //Switch players
   };
-
+  };
   // `Game.prototype.init` kicks off a new game with a board and two players
   Game.prototype.init = function() {
     //
   };
 
-  // A starter Player constructor.
-  function Player(team) {
-    //Is the player X or O?
-    //this.team = ...
-  };
-
+  
   // A starter Board constructor.
   function Board() {
+    this.$cells = (".box")
+    this.$cells = $(".box").click(function(){
+       
+    }
+
     //Tracks the cells of the board instance
     //this.$cells = ...
 
@@ -41,3 +51,5 @@
   // Start the game!
   var game = new Game();
   game.init();
+
+});
